9f3ae8cf9e301d7c2b498cc1ad2062397f1daf77
RSB-Rimon <rimonboydho7@gmail.com>
2025-07-25 22:42:39 +0600

    API

---
+++ script.js
@@ -0,0 +1,20 @@
+const imgContainer = document.querySelector("#image-container");
+const urlAPI = "https://picsum.photos/v2/list";
+const getPicture = () =>
+  fetch(urlAPI)
+    .then((data) => data.json())
+    .then((items) => {
+      //   console.log(items);
+      items.forEach(function (photObject) {
+        const newContainer = document.createElement("div");
+        newContainer.classList.add("image-container-item");
+        const newImg = document.createElement("img");
+        newImg.src = photObject.download_url;
+        newContainer.appendChild(newImg);
+
+        const newAuthor = document.createElement("p");
+        newAuthor.textContent = photObject.author;
+        newContainer.appendChild(newAuthor);
+      });
+    });
+getPicture();
